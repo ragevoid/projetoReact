9b1596aba63fc1db6399973586d7d2a8c498fffa
ragevoid <riccardo.escoche@gmail.com>
2024-12-09 08:27:33 -0300

    Alteraçao de user

--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 
 const TeamInfo = () => {
   return (
-    <Container className="d-flex justify-content-center flex-column align-items-center gap-3">
+    <div className="d-flex justify-content-center flex-column align-items-center gap-3" >
       <div className="d-flex justify-content-center align-items-center gap-3">
         <img src="src\assets\img\mengao.png" alt="Logo Flamengo" width="50" />
         <h2 className="m-0">Clube de Regatas do Flamengo</h2>
       </div>
-    </Container>
+    </div>
   );
 };
 
